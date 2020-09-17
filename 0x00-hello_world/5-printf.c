ecb548046932e4d06187f9dfe4dd84acc6d5319e
MillicentMal <millicentmalinga19@gmail.com>
2020-09-17 18:19:09 +0000

    l

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
 *main - Prints a string
-*Desription:Prints with proper grammar, but the outcome is a piece of art,
-*return: 0 (pass) 
+*
+*Return:always 0 (success)
 */
-
 int main(void)
 {
-  printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
