3872b6846c63cf24dc71ac74fb2b83d22f0ddf39
MillicentMal <millicentmalinga19@gmail.com>
2020-09-17 18:10:56 +0000

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
 /**
 *main - Prints a string
 *
-*return: 0 (sUCCESSFUL)
+*Return:always 0 (sUCCESS)
 */
 
 int main(void)
-
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
