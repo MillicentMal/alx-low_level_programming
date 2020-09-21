fd62fd47e6c31871cc75073b11e9e21d0ac63289
MillicentMal <millicentmalinga19@gmail.com>
2020-09-21 20:04:18 +0000

    fa

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,8 +20,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
 else if
 (last_dig == 0)
 printf("Last digit of %d is %d and is zero\n", n, last_dig);
-else if
-(last_dig > 0 && last_dig < 6)
+else 
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_dig);
 return (0);
 }
