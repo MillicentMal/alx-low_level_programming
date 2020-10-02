6410f0d90bd4c412999b9008a6999ac7877f2847
MillicentMal <millicentmalinga19@gmail.com>
2020-10-02 22:00:55 +0000

    l

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - prints numbers
+*
+*Return: always 0 (success)
+*/
+int main(void)
+{
+int c;
+for
+(c = 48; c < 58; c++)
+{
+putchar(c);
+
+if
+(c != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return(0);
+}
