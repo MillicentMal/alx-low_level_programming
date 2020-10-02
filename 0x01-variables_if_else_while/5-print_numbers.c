21e9a86a7c14a30d0078976941f5773150040c8a
MillicentMal <millicentmalinga19@gmail.com>
2020-10-02 20:13:57 +0000

    l

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - print alphabet in reverse
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+printf("%i", i);
+printf("\n");
+return(0);
+}
