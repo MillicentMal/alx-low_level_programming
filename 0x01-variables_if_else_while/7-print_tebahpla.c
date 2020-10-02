7d23fab8d97bd3a4771760627e8aa34d900f7a6d
MillicentMal <millicentmalinga19@gmail.com>
2020-10-02 20:57:56 +0000

    L

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - print alphabet in reverse
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+char ch;
+ch = 'z';
+while
+(ch >= 'a')
+{
+putchar(ch);
+ch--;
+}
+putchar('\n');
+return (0);
+}
