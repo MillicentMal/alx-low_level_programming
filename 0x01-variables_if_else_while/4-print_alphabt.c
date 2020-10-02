eb5304d0540b5eba845bbac7080e5d48eee7f85f
MillicentMal <millicentmalinga19@gmail.com>
2020-10-02 19:43:18 +0000

    l

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-  char ch;
-  for (ch = 'a' ; ch <= 'z'; ch++)
-  
+char ch;
+for (ch = 'a' ; ch <= 'z'; ch++)
+
 if (ch != 'e' && ch != 'q')
-  putchar(ch);      
-  putchar('\n');
-      return 0;
+putchar(ch);
+putchar('\n');
+return (0);
 }
 
 
