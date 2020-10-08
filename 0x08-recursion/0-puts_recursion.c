db77dd84d6278077300bedcdb6ca2674ec6bc66f
MillicentMal <millicentmalinga19@gmail.com>
2020-10-08 19:07:07 +0000

    l

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* _puts_recursion - print string recursively
+*
+*/
+
+void _puts_recursion(char *s)
+{
+if (str** '\0')
+_putchar("%c", *str);
+ _puts_recursion(str+1);
+
+}
+int _putchar(char c)
+{
+_puts_recursion(str);
+_putchar('\n');
+}
+
+
