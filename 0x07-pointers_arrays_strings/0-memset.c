9151dab210835289a77c6bd31594bd0649d7f157
MillicentMal <millicentmalinga19@gmail.com>
2020-10-11 06:57:55 +0000

    l

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,9 +6,9 @@
 *@n:number of bytes.
 *Return:Pointer to the memory area s.
 */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+
 char *ptr = s;
 while (n--)
 *s++ = b;
