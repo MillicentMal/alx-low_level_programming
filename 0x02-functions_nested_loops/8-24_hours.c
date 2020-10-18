ae6a0c4c26832e23cd598d9e294bde36e4a139b5
MillicentMal <millicentmalinga19@gmail.com>
2020-10-18 17:51:27 +0000

    g

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,62 +8,30 @@
 
 void jack_bauer(void)
 {
-	int i, j, k, m;
+int i, j, k, m;
 
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (m = 0; m <= 9; m++)
-				{
-					if (i == 2 && j > 3)
-					{break;
-					}
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(m + '0');
-					_putchar('\n');
-				}
-			}
-		}
-	}
-}
-#include "holberton.h"
-/**
- *jack_bauer-prints all minutes in a day from 00:00
- *
- *Return:0 if succeesful.
- */
+for (i = 0; i <= 2; i++)
 
-
-void jack_bauer(void)
 {
-	int i, j, k, m;
+for (j = 0; j <= 9; j++)
+{
+for (k = 0; k <= 5; k++)
+{
+for (m = 0; m <= 9; m++)
+{
+if (i == 2 && j > 3)
 
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (m = 0; m <= 9; m++)
-				{
-					if (i == 2 && j > 3)
-					{break;
-					}
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(m + '0');
-					_putchar('\n');
-				}
-			}
-		}
-	}
+{break;
+}
+_putchar(i + '0');
+_putchar(j + '0');
+_putchar(':');
+_putchar(k + '0');
+_putchar(m + '0');
+_putchar('\n');
+}
+}
+}
+}
 }
 
