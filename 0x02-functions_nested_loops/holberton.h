51abf2a28ec0a2ce00f3c7e3515d58309ae223c0
MillicentMal <millicentmalinga19@gmail.com>
2020-10-18 17:32:16 +0000

    four

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,5 +7,23 @@ int _putchar(char);
 /* 1-alphabet.c */
 void print_alphabet(void);
 
+void times_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+
+
+
+
+
+
+
+
+
+
+
 void print_alphabet_x10(void);
 #endif
