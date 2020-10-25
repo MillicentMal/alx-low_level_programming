ad179143ea68110aa04bbccc4ef839e725040a16
MillicentMal <millicentmalinga19@gmail.com>
2020-10-25 08:54:36 +0000

    AH

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* _putchar.c */
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+/* 1-alphabet.c */
+void print_alphabet(void);
+#endif
+
