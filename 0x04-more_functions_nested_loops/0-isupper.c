ca2574ae08dab1255e68aca2378481cf49b1f931
MillicentMal <millicentmalinga19@gmail.com>
2020-10-31 10:30:10 +0000

    one more

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 /**
 *_isupper - checks if uppercase
+*@c: value to be checked
 *Return: 1 always (success)
 *
 */
-int _isupper(int c)
+int _isupper (int c)
 {
 if (c >= 65 && c <= 90)
 {
