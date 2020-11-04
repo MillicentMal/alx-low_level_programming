8428202afe5db35a4e3e6acafd3bd2f90af35bb9
MillicentMal <millicentmalinga19@gmail.com>
2020-11-04 20:17:53 +0000

    first commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+*print_listint - prints list of elements
+*@count: number of nodes 
+*Return: count
+*@n: integer 
+*/
+size_t print_listint(const listint_t *h)
+{
+int n = 0;
+
+unsigned int listint_t *temp = h;
+
+while (temp != NULL)
+{
+n++;
+temp->next;
+
+}
+printf("%d\n",temp->next);
+temp = temp->next;
+return(n);
+}
+
