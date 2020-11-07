80ac3b23f7c583a832285bbbf6054d8397eebffc
MillicentMal <millicentmalinga19@gmail.com>
2020-11-07 02:22:45 +0000

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+*swap_int - swaps the values of two integers using their pointers
+*@a: pointer of int to be swapped
+*@b: pointer of int to be swapped
+*/
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*b = *a;
+}
+
