1f73b80ec42ca03ba204bb99389ef2841b412167
MillicentMal <millicentmalinga19@gmail.com>
2020-11-07 03:58:51 +0000

    fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,32 @@
 #include "holberton.h"
 /**
+*_strlen - checks for and returns length of string
+*Return: length of string
+*@s: string to be checked
+*
+*/
+int _strlen(char *s)
+{
+int counter;
+for (counter = 0; s[counter] != '\0'; counter++)
+{
+}
+return (counter);
+}
+
+/**
 *rev_string - reverses string
 *@s: string to be reversed
-*
 */
 void rev_string(char *s)
 {
-int count, len, temp;
-len = 0;
-while (s[len] != '\0')
-{
-len++;
-}
-len -= 1;
-for (count = 0; count < len; count++)
-{
-temp = s[len];
-s[len] = s[count];
-s[count] = temp;
+int counter = _strlen(s);
+int n = 0;
+char temp;
+while (n < counter / 2)
+temp *(s + n);
+*(s + n) = *(s + counter - 1 - n);
+*(s + counter - 1 - n) = temp;
+n++;
 }
 }
