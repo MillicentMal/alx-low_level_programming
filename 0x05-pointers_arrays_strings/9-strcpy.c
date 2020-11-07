860ce9ef3161bc9d705d47f2529fc3d1abb86fd4
MillicentMal <millicentmalinga19@gmail.com>
2020-11-07 03:19:52 +0000

    Last one

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+**_strcpy- copies string from src to dest
+*@src: pointer to string to be copied
+*@dest: pointer to new string
+*Return: pointer to new string dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+int count;
+for (count = 0; count <= '\0'; count++)
+{
+*dest = *src
+}
+return (dest);
+}
+
