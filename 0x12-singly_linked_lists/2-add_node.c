4f8f700388ef001a79863f3abb4e8b83a3f47a92
MillicentMal <millicentmalinga19@gmail.com>
2020-11-10 23:15:30 +0000

    ...

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,39 +1,43 @@
 #include "lists.h"
 #include <string.h>
 /**
- *add_node - Adds a node at the begining of a linked list.
- *@head: Pointer to a pointer that points to the head of the linked list.
- *@str: String to assgin to the element str of the linked list.
- *Return: Returns a node of type list_t.
+ *add_node - Adds a new node at the beginning of a list.
+ *@head: A pointer to the head of the structure list_t list.
+ *@str: string to be added to the structure list_t list.
+ *
+ *Return: If the function fails - NULL.
+ *        Otherwise - Address of the new element.
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *newNode = malloc(sizeof(list_t));
-if (newNode == NULL)
+char *duplicate;
+int length;
+list_t *newptr;
+
+newptr = malloc(sizeof(list_t));
+if (newptr == NULL)
+return (NULL);
+
+duplicate = strdup(str);
+if (duplicate == NULL)
 {
+free(newptr);
 return (NULL);
 }
-newNode->str = strdup(str);
-newNode->len = _strlen(str);
-newNode->next = *head;
-*head = newNode;
-return (newNode);
+
+length = 0;
+while (str[length])
+
+length++;
+
+newptr->str = duplicate;
+newptr->len = length;
+newptr->next = *head;
+
+*head = newptr;
+
+return (newptr);
+
 }
 
-/**
- *_strlen-Finds the length of a string.
- *@s:String pointer to the string whose length is to be found.
- *
- *Return: returns the length of the string.
- */
-
-int _strlen(const char *s)
-{
-int p = 0;
-/*incremeant up to when the last character is NULL,\0*/
-while (*(s + p) != 0)
-{
-p++;
-}
-return (p);
-}
