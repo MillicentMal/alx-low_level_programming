78ebecd86e2d9a69b841933bc78673159871384f
MillicentMal <millicentmalinga19@gmail.com>
2020-11-10 00:19:06 +0000

    ready

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,6 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
 
+void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
 
 int largest_number(int a, int b, int c);
 #endif
