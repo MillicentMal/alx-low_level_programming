13ae60ec0746e2562e3e51dd33269f017fed5c39
MillicentMal <millicentmalinga19@gmail.com>
2020-11-10 22:37:28 +0000

    fix error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,45 @@
 #include "lists.h"
 
 /**
- *insert_nodeint_at_index-Inserts a node at specified index.
- *@head:Pointer to a pointer that points to the head node.
- *@idx:Index where to add the new node.
- *@n:Element n of the new node.
- *Return:Pointer to the newly added node.
+ *insert_nodeint_at_index - Inserts a node at a given position.
+ *@head: Pointer to the listint_t.
+ *@idx: Index to add new nodes. starts at 0.
+ *@n: Number of nodes.
+ *
+ *Return: If the function fails - NULL
+ *        Otherwise - Address of the new node.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *temp = malloc(sizeof(listint_t));
-listint_t *navigate = *head;
-unsigned int i = 0;
+listint_t *newNode, *temp = *head;
+unsigned int nodeint = 0;
 
-temp->n = n;
-temp->next = NULL;
-if (temp == NULL && idx > 0)
-{
-free(temp);
+newNode = malloc(sizeof(listint_t));
+if (newNode == NULL)
 return (NULL);
-}
+
+newNode->n = n;
 
 if (idx == 0)
 {
-temp->next = *head;
-*head = temp;
-return (temp);
-}
-for (i = 0; i < (idx - 1); i++)
-{
-if (navigate != NULL)
-{
-navigate = navigate->next;
-}
-}
-if (navigate == NULL)
-{
-free(navigate);
-return (NULL);
-}
-else
-{
-temp->next = navigate->next;
-navigate->next = temp;
-return (temp);
-}
-free(temp);
+
+*head = newNode;
+newNode->next = temp;
+return (newNode);
 }
 
+while (nodeint < (idx - 1))
+{
+nodeint++;
+if (temp == NULL || temp->next == NULL)
+return (NULL);
+
+temp = temp->next;
+}
+
+newNode->next = temp->next;
+temp->next = newNode;
+
+return (newNode);
+}
