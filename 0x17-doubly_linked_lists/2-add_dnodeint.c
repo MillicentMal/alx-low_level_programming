1bf416b2c5bae1fb4f4f69d5a73e64c1331ce855
MillicentMal <millicentmalinga19@gmail.com>
2021-06-28 16:39:21 +0000

    yoh

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,32 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
+#include "lists.h"
+
 /**
- *add_dnodeint - a function that adds a new node at
- *the beginning of list
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @head: points to the head node
- *
- * Return:the address of new_node or NULL if failed
+ *add_dnodeint - Adds a node at the head of a doubly linked list.
+ *@head: Pointer to a pointer that points to the head of a doubly
+ *linked list.
+ *@n:element of the node to be inserted.
+ *Return: Address of the new node.
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-/*checks if new_node is there*/
-if (!new_node)
-{
-return (NULL);
-}
-/*allocates memory for new_node*/
+	dlistint_t *newNode = malloc(sizeof(dlistint_t));
 
-struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
-/*assigns next pointer to head*/
-new_node->next = (*head);
-/*assigns NULL to prev pointer*/
-new_node->prev = NULL;
-/*if head is not null then the previous for head becomes new_node*/
-if ((*head) != NULL)
-(*head)->prev = new_node;
-*head = new_node;
-return (new_node);
+	/*Check if malloc  failed*/
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+/*Assing data to the new node*/
+	newNode->n = n;
+
+	if (!*head)
+	{
+		newNode->next = NULL;
+		*head = newNode;
+		return (newNode);
+	}
+	else
+	{
+		newNode->next = *head;
+		(*head)->prev = newNode;
+		*head = newNode;
+		return (newNode);
+	}
 }
