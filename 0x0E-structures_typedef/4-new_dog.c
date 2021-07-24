466e2eb9f243e14a87e963b3e9d1eb42e90cfe58
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-07-24 10:38:56 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,15 +13,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (!new_dog || !name || !owner)
+	{
 		return (NULL);
+	}
 	n = malloc(_strlen(name) + 1);
 	if (!n)
+	{
 		return (free(new_dog), NULL);
+	}
 	n = _strdup(name);
 	new_dog->name = n;
 	o = malloc(_strlen(owner) + 1);
 	if (!o)
+	{
 		return (free(new_dog->name), free(new_dog), NULL);
+	}
 	o = _strdup(owner);
 	new_dog->owner = o;
 	new_dog->age = age;
