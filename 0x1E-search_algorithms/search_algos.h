699e628e903e848c3c9d73d0bcf82d66a4b361bc
MillicentMal <millicentmalinga19@gmail.com>
2021-09-02 15:37:02 +0000

    Search algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* binary_search */
+int binary_search(int *array, size_t size, int value);
+/*linear_search */
+int linear_search(int *array, size_t size, int value);
+
+#endif
