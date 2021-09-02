f1610800992200ecb9edd3b7b4892702fad5c256
MillicentMal <millicentmalinga19@gmail.com>
2021-09-02 16:40:54 +0000

    Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+*linear_search - does a linear search on an array of size size
+*@size: the number of elements in array
+*@value: the value to search for
+*Return: index
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+int i = 0;
+for (i = 0; i < size; i++) {
+if (array[i] == value) 
+{
+return(i);
+}
+elif (array[i] != value || *array == NULL)
+{
+return(-1);
+}
+}
+}
 
