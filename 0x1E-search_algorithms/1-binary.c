2de7b920fac1642d6033ceb6983ace22c342875e
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-09-13 13:08:31 +0200

    Create 1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int binary_search(int *array, size_t size, int value){
+int start_index = 0;
+int end_index = size;
+   while (start_index <= end_index){
+      int middle = start_index + (end_index- start_index )/2;
+      if (array[middle] == element)
+         return middle;
+      if (array[middle] < element)
+         start_index = middle + 1;
+      else
+         end_index = middle - 1;
+   }
+   return -1;
+}
