aebe303e9fb450f529c50fd30c2add64065c4e50
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-09-13 13:39:39 +0200

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,53 +9,16 @@
  *Return: -1 if value not found, else index of the found element
  */
 
-int binary_search(int *array, size_t size, int value)
-{
-	int start = 0;
-	int last = size - 1;
-	int middle;
-
-	if (array == NULL)
-		return (-1);
-
-	print_array(array, start, last);
-	while (start <= last)
-	{
-		middle = (start + last)// 2;
-		if (array[middle] == value)
-			return (middle);
-		else if (array[middle] < value)
-		{
-			print_array(array, middle + 1, start);
-			last = middle + 1;
-		}
-		else if (array[middle] > value)
-		{
-			print_array(array, last, middle - 1);
-			start = middle - 1;
-		}
-	}
-	return (-1);
-}
-
-/**
- *print_array-Prints array elements of a given array.
- *@start:First index from where the printing should begin.
- *@last: Last index to be printed.
- *@array: Array to print.
- */
-
-void print_array(int *array, int start, int last)
-{
-	int i;
-
-	if (start == last)
-		printf("Searching in array: %d\n", array[start]);
-	else if (start < last)
-	{
-		printf("Searching in array: ");
-		for (i = start; i < last; i++)
-			printf("%d, ", array[i]);
-		printf("%i\n", array[i]);
-	}
+#include <stdio.h>
+int iterativeBinarySearch(int array[], int start_index, int end_index, int value){
+   while (start_index <= end_index){
+      int middle = start_index + (end_index- start_index )/2;
+      if (array[middle] == value)
+         return middle;
+      if (array[middle] < value)
+         start_index = middle + 1;
+      else
+         end_index = middle - 1;
+   }
+   return -1;
 }
