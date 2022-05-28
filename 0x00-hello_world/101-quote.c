c6f587f622ed26ba9d73c6911ceec9f13c7b95c6
MillicentMal <millicentmalinga19@gmail.com>
2022-05-28 16:38:04 +0200

    commiitng

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,8 @@
 int main (void) {
 
 
-char s[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char s[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 write (STDOUT_FILENO, s, sizeof(s));
+write(STDOUT_FILENO, '\n', 1);
 return (1);
 }
