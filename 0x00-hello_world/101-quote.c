8f41eb44ca8db665277b590f3d9e73ce0fefb1d0
MillicentMal <millicentmalinga19@gmail.com>
2022-05-28 17:24:25 +0200

    last attempt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include<unistd.h>
+#include <stdio.h>
 /**
  *main- Entry point
  *
- *Return: Always 1 (success)
+ *Return: Always 1(failure)
  */
 int main(void)
 {
 char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(STDOUT_FILENO, s, sizeof(s));
+fprintf(stderr, s, sizeof(s));
 return (1);
 }
